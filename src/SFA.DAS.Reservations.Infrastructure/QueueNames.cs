0added1962a7dd6a703b9375ab3c8a07664da182
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-03-05 10:05:11 +0000

    Correct queue naming

--- src/SFA.DAS.Reservations.Infrastructure/QueueNames.cs
+++ src/SFA.DAS.Reservations.Infrastructure/QueueNames.cs
@@ -2,7 +2,6 @@
 {
     public static class QueueNames
     {
-        public const string RefreshCourse = "sfa-das-rsrv-get-course";
         public const string GetCourses = "sfa-das-rsrv-get-course";
         public const string StoreCourse = "sfa-das-rsrv-store-course";
     }
