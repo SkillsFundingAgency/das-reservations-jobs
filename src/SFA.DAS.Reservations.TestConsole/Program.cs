900f3c1762c0a751230152dfdbdb681d74aeb7b6
Robin O'Connor <github@rocketmansoftware.com>
2019-04-30 10:54:51 +0100

    Added test console to test nservicebus trigger code

---
+++ src/SFA.DAS.Reservations.TestConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using SFA.DAS.Reservations.Domain.Reservations;
+
+namespace SFA.DAS.Reservations.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run().Wait();
+        }
+
+        public static async Task Run()
+        {
+            var endpointConfiguration = new EndpointConfiguration("testQueue");
+
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            var command = string.Empty;
+
+            do
+            {
+                Console.WriteLine("Enter 'q' to exit..." + Environment.NewLine);
+                Console.Write("Reservation ID: ");
+                var guidStr = Console.ReadLine();
+
+                var reservationId = Guid.Parse(guidStr);
+
+                await endpointInstance.SendLocal(new ConfirmReservationMessage {ReservationId = reservationId});
+
+                Console.WriteLine("Message sent...");
+
+            } while (!command.Equals("q"));
+            
+
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
