2d995f880c1254319084a1fecd2f1b8be6239cad
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-05-16 10:46:05 +0100

    Add storing of legal entity repository call

---
+++ src/SFA.DAS.Reservations.Data/Repository/AccountLegalEntityRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SFA.DAS.Reservations.Domain.Entities;
+
+namespace SFA.DAS.Reservations.Data.Repository
+{
+    public class AccountLegalEntityRepository
+    {
+        private readonly IReservationsDataContext _dataContext;
+
+        public AccountLegalEntityRepository(IReservationsDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task Add(AccountLegalEntity accountLegalEntity)
+        {
+            using (var transaction = _dataContext.Database.BeginTransaction())
+            {
+                await _dataContext.AccountLegalEntities.AddAsync(accountLegalEntity);
+                _dataContext.SaveChanges();
+                transaction.Commit();
+            }
+        }
+    }
+}
