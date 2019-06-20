b72763d8053eb70072354d5ef077374158ad6cd0
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-06-20 11:29:37 +0100

    Update Endpoint name for confirm reservation

--- src/SFA.DAS.Reservations.Infrastructure/QueueNames.cs
+++ src/SFA.DAS.Reservations.Infrastructure/QueueNames.cs
@@ -4,7 +4,7 @@
     {
         public const string GetCourses = "sfa-das-rsrv-get-course";
         public const string StoreCourse = "sfa-das-rsrv-store-course";
-        public const string ConfirmReservation = "sfa-das-rsrv-confirm-reservation";
+        public const string ConfirmReservation = "SFA.DAS.Reservations.ConfirmReservation";
         public const string LegalEntityAdded = "SFA.DAS.Reservations.Jobs.LegalEntityAdded";
         public const string RemovedLegalEntity = "SFA.DAS.Reservations.Jobs.LegalEntityRemoved";
         public const string SignedAgreement = "SFA.DAS.Reservations.Jobs.SignedAgreement";
