6e5fc6a185bbe8da57eb8a283fd2dc55af4e70a9
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-06-28 06:38:48 +0100

    Shorten Endpoint name

--- src/SFA.DAS.Reservations.Infrastructure/QueueNames.cs
+++ src/SFA.DAS.Reservations.Infrastructure/QueueNames.cs
@@ -8,6 +8,6 @@
         public const string LegalEntityAdded = "SFA.DAS.Reservations.Jobs.LegalEntityAdded";
         public const string RemovedLegalEntity = "SFA.DAS.Reservations.Jobs.LegalEntityRemoved";
         public const string SignedAgreement = "SFA.DAS.Reservations.Jobs.SignedAgreement";
-        public const string DraftApprenticeshipDeleted = "SFA.DAS.Reservations.Jobs.DraftApprenticeshipDeleted";
+        public const string DraftApprenticeshipDeleted = "SFA.DAS.Reservations.Jobs.ApprenticeshipDeleted";
     }
 }
