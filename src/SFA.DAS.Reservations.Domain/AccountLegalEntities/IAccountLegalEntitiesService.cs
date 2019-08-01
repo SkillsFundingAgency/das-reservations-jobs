fb6d23ec18cc8aefd71bb5c44ef3670d0aea80e8
Akshay Chugh <akshay.chugh30@outlook.com>
2019-08-01 11:33:24 +0100

    Updated account legal entity service interface

--- src/SFA.DAS.Reservations.Domain/AccountLegalEntities/IAccountLegalEntitiesService.cs
+++ src/SFA.DAS.Reservations.Domain/AccountLegalEntities/IAccountLegalEntitiesService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using SFA.DAS.EmployerAccounts.Messages.Events;
+using SFA.DAS.EmployerFinance.Messages.Events;
 
 namespace SFA.DAS.Reservations.Domain.AccountLegalEntities
 {
@@ -8,5 +9,6 @@ namespace SFA.DAS.Reservations.Domain.AccountLegalEntities
         Task AddAccountLegalEntity(AddedLegalEntityEvent accountLegalEntity);
         Task SignAgreementForAccountLegalEntity(EmployerAccounts.Messages.Events.SignedAgreementEvent signedAgreementEvent);
         Task RemoveAccountLegalEntity(RemovedLegalEntityEvent accountLegalEntityRemovedEvent);
+        Task UpdateAccountToLevy(LevyAddedToAccount levyAddedToAccountEvent);
     }
 }
