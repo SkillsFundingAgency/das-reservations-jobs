31156d417c02bb392f675f658a9a2bc47976f433
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-08-15 15:11:10 +0100

    Add QueueMonitor domain item and test

---
+++ src/SFA.DAS.Reservations.Domain.UnitTests/Infrastructure/WhenBuildingAQueueMonitorItem.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using SFA.DAS.Reservations.Domain.Infrastructure;
+
+namespace SFA.DAS.Reservations.Domain.UnitTests.Infrastructure
+{
+    public class WhenBuildingAQueueMonitorItem
+    {
+        [Test]
+        public void Then_The_Messages_Are_Set_On_The_Model()
+        {
+            //Act
+            var actual = new QueueMonitor("queue.name", true);
+
+            //Assert
+            Assert.AreEqual("queue.name has entered an error state", actual.QueueErrorMessage);
+            Assert.AreEqual("queue.name has no errors", actual.QueueNoErrorMessage);
+        }
+    }
+}
---
+++ src/SFA.DAS.Reservations.Domain/Infrastructure/QueueMonitor.cs
@@ -0,0 +1,19 @@
+﻿namespace SFA.DAS.Reservations.Domain.Infrastructure
+{
+    public class QueueMonitor
+    {
+        public string QueueName { get; }
+
+        public bool? IsError { get; }
+        public string QueueErrorMessage { get; }
+        public string QueueNoErrorMessage { get; }
+
+        public QueueMonitor(string queueName, bool? isError)
+        {
+            QueueName = queueName;
+            IsError = isError;
+            QueueErrorMessage = $"{QueueName} has entered an error state";
+            QueueNoErrorMessage = $"{QueueName} has no errors";
+        }
+    }
+}
