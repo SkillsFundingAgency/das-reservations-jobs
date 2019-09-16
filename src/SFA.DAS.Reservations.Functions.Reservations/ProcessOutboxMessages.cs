7268dd8444476e7a4022948abc5e937d922e4d48
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-09-16 16:06:49 +0100

    Add outbox processing

---
+++ src/SFA.DAS.Reservations.Functions.Reservations/ProcessOutboxMessages.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using SFA.DAS.NServiceBus.ClientOutbox;
+using SFA.DAS.Reservations.Infrastructure.Attributes;
+
+namespace SFA.DAS.Reservations.Functions.Reservations
+{
+    public class ProcessOutboxMessages
+    {
+        [FunctionName("ProcessOutboxMessages")]
+        public static async Task Run([TimerTrigger("0 */10 * * * *")] TimerInfo timer, [Inject] IProcessClientOutboxMessagesJob handler)
+        {
+            await handler.RunAsync();
+        }
+    }
+}
