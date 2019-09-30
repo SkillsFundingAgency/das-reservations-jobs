8a28886efec49b0458d1ecb32f67cc4616e420e7
Scott Wakefield <scott@expertcc.co.uk>
2019-09-30 15:06:56 +0100

    MF-16 added provider service and map from api type.

---
+++ src/SFA.DAS.Reservations.Domain.UnitTests/Providers/WhenCastingProviderDetailsFromApiType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Apprenticeships.Api.Types.Providers;
+using SFA.DAS.Reservations.Domain.Providers;
+
+namespace SFA.DAS.Reservations.Domain.UnitTests.Providers
+{
+    public class WhenCastingProviderDetailsFromApiType
+    {
+        [Test, AutoData]
+        public void Then_Maps_Matching_Fields(Provider provider)
+        {
+            ProviderDetails providerDetails = provider;
+
+            providerDetails.Should().BeEquivalentTo(provider, options => 
+                options.ExcludingMissingMembers());
+            providerDetails.ProviderId.Should().Be(Convert.ToUInt32(provider.Ukprn));
+        }
+    }
+}
--- src/SFA.DAS.Reservations.Domain/Providers/ProviderDetails.cs
+++ src/SFA.DAS.Reservations.Domain/Providers/ProviderDetails.cs
@@ -1,6 +1,42 @@
-﻿namespace SFA.DAS.Reservations.Domain.Providers
+﻿using System;
+using SFA.DAS.Apprenticeships.Api.Types.Providers;
+
+namespace SFA.DAS.Reservations.Domain.Providers
 {
     public class ProviderDetails
     {
+        public uint ProviderId { get; set; }
+        public bool IsHigherEducationInstitute { get; set; }
+        public string ProviderName { get; set; }
+        public bool CurrentlyNotStartingNewApprentices { get; set; }
+        public bool IsEmployerProvider { get; set; }
+        public string Uri { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool NationalProvider { get; set; }
+        public string Website { get; set; }
+        public bool HasParentCompanyGuarantee { get; set; }
+        public bool IsNew { get; set; }
+        public bool IsLevyPayerOnly { get; set; }
+
+        public static implicit operator ProviderDetails(Provider provider)
+        {
+            return new ProviderDetails
+            {
+                ProviderId = Convert.ToUInt32(provider.Ukprn),
+                IsHigherEducationInstitute = provider.IsHigherEducationInstitute,
+                ProviderName = provider.ProviderName,
+                CurrentlyNotStartingNewApprentices = provider.CurrentlyNotStartingNewApprentices,
+                IsEmployerProvider = provider.IsEmployerProvider,
+                Uri = provider.Uri,
+                Phone = provider.Phone,
+                Email = provider.Email,
+                NationalProvider = provider.NationalProvider,
+                Website = provider.Website,
+                HasParentCompanyGuarantee = provider.HasParentCompanyGuarantee,
+                IsNew = provider.IsNew,
+                IsLevyPayerOnly = provider.IsLevyPayerOnly
+            };
+        }
     }
 }
