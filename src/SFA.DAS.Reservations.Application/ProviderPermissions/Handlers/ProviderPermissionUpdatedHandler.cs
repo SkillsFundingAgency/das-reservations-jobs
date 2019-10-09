2419e297ae8f46e59873f9952dd6be0e32573a92
Robin O'Connor <github@rocketmansoftware.com>
2019-10-09 11:53:20 +0100

    Added initial event trigger for provider permission update event

---
+++ src/SFA.DAS.Reservations.Application/ProviderPermissions/Handlers/ProviderPermissionUpdatedHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.ProviderRelationships.Messages.Events;
+using SFA.DAS.Reservations.Domain.ProviderPermissions;
+
+namespace SFA.DAS.Reservations.Application.ProviderPermissions.Handlers
+{
+    public class ProviderPermissionUpdatedHandler : IProviderPermissionUpdatedHandler
+    {
+        public Task Handle(UpdatedPermissionsEvent updateEvent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SFA.DAS.Reservations.Functions.ProviderPermission/HandleProviderPermissionUpdatedEvent.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.NServiceBus.AzureFunction.Infrastructure;
+using SFA.DAS.ProviderRelationships.Messages.Events;
+using SFA.DAS.Reservations.Domain.ProviderPermissions;
+
+namespace SFA.DAS.Reservations.Functions.ProviderPermission
+{
+    
+    public class HandleProviderPermissionUpdatedEvent
+    {
+        [FunctionName("HandleProviderPermissionUpdatedEvent")]
+        public static async Task Run([NServiceBusTrigger(EndPoint = "")] UpdatedPermissionsEvent updateEvent, ILogger<UpdatedPermissionsEvent> logger, IProviderPermissionUpdatedHandler providerPermissionUpdatedHandler)
+        {
+            logger.LogInformation($"NServiceBus AddedAccountProvider trigger function executed at: {DateTime.Now}");
+            await providerPermissionUpdatedHandler.Handle(updateEvent);
+        }
+    }
+}
