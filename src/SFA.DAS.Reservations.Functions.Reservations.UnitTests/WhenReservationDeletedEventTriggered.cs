08f329808d656da7e49598d89a04f87b8b9b11b0
Scott Wakefield <scott@expertcc.co.uk>
2019-10-18 09:59:14 +0100

    MF-34 added new function for deleted reservation.

---
+++ src/SFA.DAS.Reservations.Functions.Reservations.UnitTests/WhenReservationDeletedEventTriggered.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Reservations.Domain.Reservations;
+using SFA.DAS.Reservations.Messages;
+
+namespace SFA.DAS.Reservations.Functions.Reservations.UnitTests
+{
+    public class WhenReservationDeletedEventTriggered
+    {
+        [Test, AutoData]
+        public async Task Then_Notification_Action_Executed(ReservationDeletedEvent deletedEvent)
+        {
+            //Arrange
+            var handler = new Mock<INotifyEmployerWhenReservationDeletedAction>();
+
+            //Act
+            await HandleReservationDeletedEvent.Run(
+                deletedEvent, 
+                Mock.Of<ILogger<ReservationDeletedEvent>>(),
+                handler.Object);
+
+            //Assert
+            handler.Verify(s => s.Execute(deletedEvent), Times.Once);
+        }
+    }
+}
