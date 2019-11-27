18140ade479b227bc64f7ada819e4c6c2ad0e8d6
Peter Williams <peter.david.williams.1991@gmail.com>
2019-11-27 11:22:03 +0000

    added feature file for create apprenticeship

---
+++ src/SFA.DAS.Reservations.Functions.Reservations.AcceptanceTests/Features/CreateReservation.feature
@@ -0,0 +1,9 @@
+﻿Feature: Reservation created
+	In order to know when a reservation has been created
+	I want to be told when the reservation search index is updated
+
+
+Scenario: Reservation search index should be updated
+	Given I have a reservation ready for creation
+	When a create reservation event is triggered
+	Then the reservation search index should be updated
---
+++ src/SFA.DAS.Reservations.Functions.Reservations.AcceptanceTests/Steps/ReservationCreatedSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Reservations.Functions.Reservations.AcceptanceTests.Steps
+{
+    [Binding]
+    public class ReservationCreatedSteps
+    {
+        [Given(@"I have a reservation ready for creation")]
+        public void GivenIHaveAReservationReadyForCreation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"a create reservation event is triggered")]
+        public void WhenACreateReservationEventIsTriggered()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the reservation search index should be updated")]
+        public void ThenTheReservationSearchIndexShouldBeUpdated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
