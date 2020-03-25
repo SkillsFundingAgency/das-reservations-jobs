dec63325c93af911c63d611f254cdd58503a6040
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-03-25 11:55:57 +0000

    Implement missing code
    Implemented TryGetServerErrorReason for ElasticSearchResponse

--- src/SFA.DAS.Reservations.Domain/Infrastructure/ElasticSearch/ElasticSearchResponse.cs
+++ src/SFA.DAS.Reservations.Domain/Infrastructure/ElasticSearch/ElasticSearchResponse.cs
@@ -4,6 +4,46 @@ namespace SFA.DAS.Reservations.Domain.Infrastructure.ElasticSearch
 {
     public class ElasticSearchResponse : IElasticsearchResponse
     {
+        private ServerError _serverError;
+        private Error _error;
+        private int? _statusCode;
+        public ServerError ServerError
+        {
+            get
+            {
+                if (this._serverError != null)
+                    return this._serverError;
+                if (this._error == null)
+                    return (ServerError) null;
+                this._serverError = new ServerError(this._error, this._statusCode);
+                return this._serverError;
+            }
+        }
+        internal int? StatusCode
+        {
+            get
+            {
+                return this._statusCode;
+            }
+            set
+            {
+                this._statusCode = value;
+                this._serverError = (ServerError) null;
+            }
+        }
+        internal Error Error
+        {
+            get
+            {
+                return this._error;
+            }
+            set
+            {
+                this._error = value;
+                this._serverError = (ServerError) null;
+            }
+        }
+        
         public Shards _shards { get; set; }
         public string _index { get; set; }
         public string _id { get; set; }
@@ -14,7 +54,8 @@ namespace SFA.DAS.Reservations.Domain.Infrastructure.ElasticSearch
         
         public bool TryGetServerErrorReason(out string reason)
         {
-            throw new System.NotImplementedException();
+            reason = ServerError?.Error?.ToString();
+            return !string.IsNullOrEmpty(reason);
         }
 
         public IApiCallDetails ApiCall { get; set; }
