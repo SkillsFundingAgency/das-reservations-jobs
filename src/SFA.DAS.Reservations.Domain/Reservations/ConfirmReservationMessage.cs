a2e8704a40fed5fbadb4431f88bd5166e9e48cb0
Scott Wakefield <scott@expertcc.co.uk>
2020-04-23 16:22:44 +0100

    MF-514 added addtional properties to domain message.

--- src/SFA.DAS.Reservations.Domain/Reservations/ConfirmReservationMessage.cs
+++ src/SFA.DAS.Reservations.Domain/Reservations/ConfirmReservationMessage.cs
@@ -5,5 +5,7 @@ namespace SFA.DAS.Reservations.Domain.Reservations
     public class ConfirmReservationMessage  
     {
         public Guid ReservationId { get; set; }
+        public long CohortId { get; set; }
+        public long DraftApprenticeshipId { get; set; }
     }
 }
