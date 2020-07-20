b4b7e97779239fa58e8307b79615d92d7fb32666
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-20 09:48:24 +0100

    Make sure that min datetime is mapped to null

--- src/SFA.DAS.Reservations.Application.UnitTests/RefreshCourse/Services/WhenStoringCourseInformation.cs
+++ src/SFA.DAS.Reservations.Application.UnitTests/RefreshCourse/Services/WhenStoringCourseInformation.cs
@@ -36,5 +36,25 @@ namespace SFA.DAS.Reservations.Application.UnitTests.RefreshCourse.Services
                    c.Title.Equals(course.Title) &&
                    c.EffectiveTo.Equals(course.EffectiveTo))), Times.Once);
         }
+  
+        [Test]
+        public async Task Then_The_Repository_Is_Called_With_The_Mapped_Entity_And_Sets_EffectiveTo_To_Null_If_DateTime_Min()
+        {
+            //Arrange
+            var course = new Course(1,"My Course", 1, DateTime.MinValue);
+            
+            //Act
+            await _service.Store(course);
+
+            //Act
+            _repository.Verify(x=>x.Add(It.Is<Domain.Entities.Course>(
+                c=>c.CourseId.Equals(course.Id) &&
+                   c.Level.Equals(course.Level) &&
+                   c.Title.Equals(course.Title) &&
+                   c.EffectiveTo.Equals(null))), Times.Once);
+        }
+
+
+        
     }
 }
