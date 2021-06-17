873fb4ad847e4796a9de7633841657ea36b6d91e
vas <vasantha.kasirajan@digital.education.gov.uk>
2021-06-17 13:37:43 +0100

    CON-2544 - Drop Reservation Limit from AccountLegalEntity

--- src/SFA.DAS.Reservations.Domain/Entities/AccountLegalEntity.cs
+++ src/SFA.DAS.Reservations.Domain/Entities/AccountLegalEntity.cs
@@ -9,8 +9,7 @@ namespace SFA.DAS.Reservations.Domain.Entities
         public long AccountId { get; set; }
         public long LegalEntityId { get; set; }
         public long AccountLegalEntityId { get; set; }
-        public string AccountLegalEntityName { get; set; }
-        public int? ReservationLimit { get; set; }
+        public string AccountLegalEntityName { get; set; }        
         public bool AgreementSigned { get; set; }
         
     }
