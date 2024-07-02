4fd7e7148a9143d86d2071d853cb56317460e54d
Jon Harris <jon.harris@digital.education.gov.uk>
2024-07-02 10:59:27 +0100

    license

--- src/SFA.DAS.Reservations.Domain/Configuration/ReservationsJobs.cs
+++ src/SFA.DAS.Reservations.Domain/Configuration/ReservationsJobs.cs
@@ -5,6 +5,7 @@
         public string ReservationsBaseUrl { get; set; }
         public string ConnectionString { get; set; }
         public string NServiceBusConnectionString { get; set; }
+        public string NServiceBusLicense { get; set; }
         public string AzureWebJobsStorage { get; set; }
         public string ElasticSearchUsername { get; set; }
         public string ElasticSearchPassword { get; set; }
