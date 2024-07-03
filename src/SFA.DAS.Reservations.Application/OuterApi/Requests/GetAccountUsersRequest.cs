1a6fd93ae41f02a28ee73c362239dee4a1f9d681
Jon Harris <jon.harris@digital.education.gov.uk>
2024-07-03 15:40:54 +0100

    Adding GetAccountUsersRequest

---
+++ src/SFA.DAS.Reservations.Application/OuterApi/Requests/GetAccountUsersRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace SFA.DAS.Reservations.Application.OuterApi.Requests;
+
+public class GetAccountUsersRequest(long accountId) : IGetApiRequest
+{
+    public string GetUrl => $"accounts/{accountId}/users";
+}
