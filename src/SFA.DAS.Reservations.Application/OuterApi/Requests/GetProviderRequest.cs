6c90c8bc9fa49eab74a97f9bffb11085ad1be943
Jon Harris <jon.harris@digital.education.gov.uk>
2024-07-03 15:40:43 +0100

    Refactor

--- src/SFA.DAS.Reservations.Application/OuterApi/Requests/GetProviderRequest.cs
+++ src/SFA.DAS.Reservations.Application/OuterApi/Requests/GetProviderRequest.cs
@@ -1,6 +1,6 @@
 namespace SFA.DAS.Reservations.Application.OuterApi.Requests;
 
-public record GetProviderRequest(uint UkPrn) : IGetApiRequest
+public record GetProviderRequest(uint ukPrn) : IGetApiRequest
 {
-    public string GetUrl => $"providers/{UkPrn}";
+    public string GetUrl => $"providers/{ukPrn}";
 }
