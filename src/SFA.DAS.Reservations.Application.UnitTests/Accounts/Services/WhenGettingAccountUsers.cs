0a6ece826000ba5a52b2110b0925f1ebc45ae6fe
Jon Harris <jon.harris@digital.education.gov.uk>
2024-07-04 07:32:47 +0100

    Fixing tests

--- src/SFA.DAS.Reservations.Application.UnitTests/Accounts/Services/WhenGettingAccountUsers.cs
+++ src/SFA.DAS.Reservations.Application.UnitTests/Accounts/Services/WhenGettingAccountUsers.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
 using FluentAssertions;
@@ -18,46 +17,42 @@ public class WhenGettingAccountUsers
     [Test, MoqAutoData]
     public async Task Then_Calls_Api(
         long accountId,
-        Mock<IOuterApiClient> outerApiClient,
-        ICollection<AccountUser> apiTeamMembers,
+        [Frozen]Mock<IOuterApiClient> outerApiClient,
         GetAccountUsersResponse response,
         AccountsService service)
     {
-        response.AccountUsers = apiTeamMembers;
-        
-        //outerApiClient.Setup(x => x.Get<GetAccountUsersResponse>(It.Is<GetAccountUsersRequest>(y => y.Equals(accountId)))).ReturnsAsync(response);
-        outerApiClient.Setup(x => x.Get<GetAccountUsersResponse>(It.IsAny<GetAccountUsersRequest>())).ReturnsAsync(response);
+        outerApiClient
+            .Setup(x => x.Get<GetAccountUsersResponse>(It.Is<GetAccountUsersRequest>(y => y.GetUrl.Equals($"accounts/{accountId}/users"))))
+            .ReturnsAsync(response);
         
         await service.GetAccountUsers(accountId);
 
-        outerApiClient.Verify(client => client.Get<GetAccountUsersResponse>(new GetAccountUsersRequest(accountId)), Times.Once);
+        outerApiClient.Verify(client => client.Get<GetAccountUsersResponse>(It.Is<GetAccountUsersRequest>(y => y.GetUrl.Equals($"accounts/{accountId}/users") )), Times.Once);
     }
 
     [Test, MoqAutoData]
     public async Task Then_Returns_Users_From_Api(
         long accountId,
-        List<AccountUser> apiTeamMembers,
-        Mock<IOuterApiClient> outerApiClient,
+        [Frozen]Mock<IOuterApiClient> outerApiClient,
+        GetAccountUsersResponse response,
         AccountsService service)
     {
         outerApiClient
-            .Setup(client => client.Get<GetAccountUsersResponse>(new GetAccountUsersRequest(accountId)))
-            .ReturnsAsync(new GetAccountUsersResponse
-            {
-                AccountUsers = apiTeamMembers
-            });
+            .Setup(client => client.Get<GetAccountUsersResponse>(It.Is<GetAccountUsersRequest>(y => y.GetUrl.Equals($"accounts/{accountId}/users"))))
+            .ReturnsAsync(response);
 
-        var response = (await service.GetAccountUsers(accountId)).ToList();
+        var result = (await service.GetAccountUsers(accountId)).ToList();
 
-        response.Should().BeEquivalentTo(apiTeamMembers, options => options
-            .Excluding(c => c.Status)
-        );
+        result
+            .Should()
+            .BeEquivalentTo(response.AccountUsers, options => options
+            .Excluding(c => c.Status));
 
-        foreach (var exp in apiTeamMembers)
+        foreach (var accountUser in response.AccountUsers)
         {
-            response.Should().Contain(act =>
-                act.Status.Equals((byte)exp.Status)
-            );
+            result
+                .Should()
+                .Contain(userDetails => userDetails.Status.Equals((byte)accountUser.Status));
         }
     }
 }
