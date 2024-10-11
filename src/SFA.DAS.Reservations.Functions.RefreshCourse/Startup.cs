e76ea0b9e90070c825ad50d8f6638bedec1bc136
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2024-10-11 14:23:13 +0100

    missing outer api registration.

--- src/SFA.DAS.Reservations.Functions.RefreshCourse/Startup.cs
+++ src/SFA.DAS.Reservations.Functions.RefreshCourse/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging.ApplicationInsights;
 using Microsoft.Extensions.Options;
 using NLog.Extensions.Logging;
 using SFA.DAS.Configuration.AzureTableStorage;
+using SFA.DAS.Reservations.Application.OuterApi;
 using SFA.DAS.Reservations.Application.RefreshCourses.Handlers;
 using SFA.DAS.Reservations.Application.RefreshCourses.Services;
 using SFA.DAS.Reservations.Data.Repository;
@@ -23,86 +24,91 @@ using SFA.DAS.Reservations.Infrastructure.Logging;
 
 [assembly: WebJobsStartup(typeof(Startup))]
 
-namespace SFA.DAS.Reservations.Functions.RefreshCourse
+namespace SFA.DAS.Reservations.Functions.RefreshCourse;
+
+internal class Startup : IWebJobsStartup
 {
-    internal class Startup : IWebJobsStartup
+    public void Configure(IWebJobsBuilder builder)
     {
-        public void Configure(IWebJobsBuilder builder)
+        builder.AddExecutionContextBinding();
+        builder.AddDependencyInjection<ServiceProviderBuilder>();
+    }
+}
+
+internal class ServiceProviderBuilder : IServiceProviderBuilder
+{
+    private readonly ILoggerFactory _loggerFactory;
+    public IConfiguration Configuration { get; }
+
+    public ServiceProviderBuilder(ILoggerFactory loggerFactory, IConfiguration configuration)
+    {
+        _loggerFactory = loggerFactory;
+
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("local.settings.json", true)
+            .AddEnvironmentVariables();
+        if (!configuration["EnvironmentName"].Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
         {
-            builder.AddExecutionContextBinding();
-            builder.AddDependencyInjection<ServiceProviderBuilder>();
+            config.AddAzureTableStorage(options =>
+            {
+                options.ConfigurationKeys = configuration["ConfigNames"].Split(',');
+                options.StorageConnectionString = configuration["ConfigurationStorageConnectionString"];
+                options.EnvironmentName = configuration["EnvironmentName"];
+                options.PreFixConfigurationKeys = false;
+            });
         }
+
+        Configuration = config.Build();
     }
 
-    internal class ServiceProviderBuilder : IServiceProviderBuilder
+    public IServiceProvider Build()
     {
-        private readonly ILoggerFactory _loggerFactory;
-        public IConfiguration Configuration { get; }
+        var services = new ServiceCollection();
 
-        public ServiceProviderBuilder(ILoggerFactory loggerFactory, IConfiguration configuration)
+        services.Configure<ReservationsJobs>(Configuration.GetSection("ReservationsJobs"));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<ReservationsJobs>>().Value);
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var config = serviceProvider.GetService<ReservationsJobs>();
+
+        var nLogConfiguration = new NLogConfiguration();
+
+        services.AddLogging(builder =>
         {
-            _loggerFactory = loggerFactory;
-
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("local.settings.json", true)
-                .AddEnvironmentVariables();
-            if (!configuration["EnvironmentName"].Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
-            {
-                config.AddAzureTableStorage(options =>
-                {
-                    options.ConfigurationKeys = configuration["ConfigNames"].Split(',');
-                    options.StorageConnectionString = configuration["ConfigurationStorageConnectionString"];
-                    options.EnvironmentName = configuration["EnvironmentName"];
-                    options.PreFixConfigurationKeys = false;
-                });
-            }
-
-            Configuration = config.Build();
-        }
-
-        public IServiceProvider Build()
-        {
-            var services = new ServiceCollection();
-
-            services.Configure<ReservationsJobs>(Configuration.GetSection("ReservationsJobs"));
-            services.AddSingleton(cfg => cfg.GetService<IOptions<ReservationsJobs>>().Value);
-
-            var serviceProvider = services.BuildServiceProvider();
-
-            var config = serviceProvider.GetService<ReservationsJobs>();
-
-            var nLogConfiguration = new NLogConfiguration();
-
-            services.AddLogging(builder =>
-            {
-                builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
-                builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
+            builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
+            builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
                 
-                builder.SetMinimumLevel(LogLevel.Information);
+            builder.SetMinimumLevel(LogLevel.Information);
                 
-                builder.AddConsole();
-                builder.AddDebug();
-                nLogConfiguration.ConfigureNLog(Configuration);
-                builder.AddNLog(new NLogProviderOptions
-                {
-                    CaptureMessageTemplates = true,
-                    CaptureMessageProperties = true
-                });
+            builder.AddConsole();
+            builder.AddDebug();
+            nLogConfiguration.ConfigureNLog(Configuration);
+            builder.AddNLog(new NLogProviderOptions
+            {
+                CaptureMessageTemplates = true,
+                CaptureMessageProperties = true
             });
+        });
 
-            services.AddTransient<IFindApprenticeshipTrainingService, FindApprenticeshipTrainingService>();
-            services.AddTransient<IApprenticeshipCourseService, ApprenticeshipCoursesService>();
-            services.AddTransient<ICourseService, CourseService>();
+        services.AddHttpClient<IOuterApiClient, OuterApiClient>();
+        services.AddTransient<IFindApprenticeshipTrainingService, FindApprenticeshipTrainingService>();
+        services.AddTransient<IApprenticeshipCourseService, ApprenticeshipCoursesService>();
+        services.AddTransient<ICourseService, CourseService>();
 
-            services.AddTransient<IGetCoursesHandler, GetCoursesHandler>();
-            services.AddTransient<IStoreCourseHandler, StoreCourseHandler>();
+        services.AddTransient<IGetCoursesHandler, GetCoursesHandler>();
+        services.AddTransient<IStoreCourseHandler, StoreCourseHandler>();
 
-            services.AddDatabaseRegistration(config, Configuration["EnvironmentName"]);
+        services.AddDatabaseRegistration(config, Configuration["EnvironmentName"]);
 
-            services.AddTransient<ICourseRepository, CourseRepository>();
+        services.AddTransient<ICourseRepository, CourseRepository>();
 
-            return services.BuildServiceProvider();
-        }
+        return services.BuildServiceProvider();
+    }
+    
+    private static string EnsureUrlEndWithForwardSlash(string url)
+    {
+        return url.EndsWith('/') ? url : $"{url}/";
     }
 }
