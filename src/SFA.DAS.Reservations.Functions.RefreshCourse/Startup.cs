59ebc89faf396ade7ea0faa958d7f1b31c8b6262
Jon Harris <jon.harris@fuitsu.com>
2024-10-31 10:19:45 +0000

    Refactor/tidy/logging improvements

--- src/SFA.DAS.Reservations.Functions.RefreshCourse/Startup.cs
+++ src/SFA.DAS.Reservations.Functions.RefreshCourse/Startup.cs
@@ -1,113 +1,60 @@
 ﻿using System;
-using System.IO;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.ApplicationInsights;
 using Microsoft.Extensions.Options;
-using NLog.Extensions.Logging;
-using SFA.DAS.Configuration.AzureTableStorage;
-using SFA.DAS.Reservations.Application.RefreshCourses.Handlers;
-using SFA.DAS.Reservations.Application.RefreshCourses.Services;
-using SFA.DAS.Reservations.Data.Repository;
 using SFA.DAS.Reservations.Domain.Configuration;
 using SFA.DAS.Reservations.Domain.Infrastructure;
-using SFA.DAS.Reservations.Domain.RefreshCourse;
 using SFA.DAS.Reservations.Functions.RefreshCourse;
-using SFA.DAS.Reservations.Infrastructure.Api;
+using SFA.DAS.Reservations.Functions.RefreshCourse.Extensions;
 using SFA.DAS.Reservations.Infrastructure.Database;
 using SFA.DAS.Reservations.Infrastructure.DependencyInjection;
-using SFA.DAS.Reservations.Infrastructure.Logging;
 
 [assembly: WebJobsStartup(typeof(Startup))]
 
-namespace SFA.DAS.Reservations.Functions.RefreshCourse
+namespace SFA.DAS.Reservations.Functions.RefreshCourse;
+
+internal class Startup : IWebJobsStartup
 {
-    internal class Startup : IWebJobsStartup
+    public void Configure(IWebJobsBuilder builder)
     {
-        public void Configure(IWebJobsBuilder builder)
-        {
-            builder.AddExecutionContextBinding();
-            builder.AddDependencyInjection<ServiceProviderBuilder>();
-        }
+        builder.AddExecutionContextBinding();
+        builder.AddDependencyInjection<ServiceProviderBuilder>();
+    }
+}
+
+internal class ServiceProviderBuilder : IServiceProviderBuilder
+{
+    private readonly IConfiguration _configuration;
+
+    public ServiceProviderBuilder(IConfiguration configuration)
+    {
+        _configuration = configuration.BuildConfiguration();
     }
 
-    internal class ServiceProviderBuilder : IServiceProviderBuilder
+    public IServiceProvider Build()
     {
-        private readonly ILoggerFactory _loggerFactory;
-        public IConfiguration Configuration { get; }
+        var services = new ServiceCollection();
 
-        public ServiceProviderBuilder(ILoggerFactory loggerFactory, IConfiguration configuration)
-        {
-            _loggerFactory = loggerFactory;
+        services.Configure<ReservationsJobs>(_configuration.GetSection("ReservationsJobs"));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<ReservationsJobs>>().Value);
 
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("local.settings.json", true)
-                .AddEnvironmentVariables();
-            if (!configuration["EnvironmentName"].Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
-            {
-                config.AddAzureTableStorage(options =>
-                {
-                    options.ConfigurationKeys = configuration["ConfigNames"].Split(',');
-                    options.StorageConnectionString = configuration["ConfigurationStorageConnectionString"];
-                    options.EnvironmentName = configuration["EnvironmentName"];
-                    options.PreFixConfigurationKeys = false;
-                });
-            }
+        var serviceProvider = services.BuildServiceProvider();
 
-            Configuration = config.Build();
-        }
+        var config = serviceProvider.GetService<ReservationsJobs>();
 
-        public IServiceProvider Build()
-        {
-            var services = new ServiceCollection();
+        services.AddDasLogging(_configuration);
 
-            services.Configure<ReservationsJobs>(Configuration.GetSection("ReservationsJobs"));
-            services.AddSingleton(cfg => cfg.GetService<IOptions<ReservationsJobs>>().Value);
+        services.AddDatabaseRegistration(config, _configuration["EnvironmentName"]);
 
-            var serviceProvider = services.BuildServiceProvider();
+        services.AddApplicationServices();
 
-            var config = serviceProvider.GetService<ReservationsJobs>();
+        services
+            .AddApplicationInsightsTelemetryWorkerService()
+            .ConfigureFunctionsApplicationInsights();
 
-            var nLogConfiguration = new NLogConfiguration();
-
-            services.AddLogging(builder =>
-            {
-                builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
-                builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
-                
-                builder.SetMinimumLevel(LogLevel.Information);
-                
-                builder.AddConsole();
-                builder.AddDebug();
-                nLogConfiguration.ConfigureNLog(Configuration);
-                builder.AddNLog(new NLogProviderOptions
-                {
-                    CaptureMessageTemplates = true,
-                    CaptureMessageProperties = true
-                });
-            });
-
-            services.AddTransient<IFindApprenticeshipTrainingService, FindApprenticeshipTrainingService>();
-            services.AddTransient<IApprenticeshipCourseService, ApprenticeshipCoursesService>();
-            services.AddTransient<ICourseService, CourseService>();
-
-            services.AddTransient<IGetCoursesHandler, GetCoursesHandler>();
-            services.AddTransient<IStoreCourseHandler, StoreCourseHandler>();
-
-            services.AddDatabaseRegistration(config, Configuration["EnvironmentName"]);
-
-            services.AddTransient<ICourseRepository, CourseRepository>();
-            
-            services
-                .AddApplicationInsightsTelemetryWorkerService()
-                .ConfigureFunctionsApplicationInsights();
-
-            return services.BuildServiceProvider();
-        }
+        return services.BuildServiceProvider();
     }
 }
