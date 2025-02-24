3a7155057361ed15e05f0688e21c5d0c9b789966
Paul Graham <paul.graham@coprime.co.uk>
2025-02-24 12:14:41 +0000

    Added new handler

---
+++ src/SFA.DAS.Reservations.Domain/AccountLegalEntities/ILevyAddedToAccountEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.EmployerFinance.Messages.Events;
+
+namespace SFA.DAS.Reservations.Domain.AccountLegalEntities;
+
+public interface ILevyAddedToAccountEventHandler
+{
+    Task Handle(LevyAddedToAccount levyAddedToAccountEvent);
+}
