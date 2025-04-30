d84a8a3aef7544e8032d372f426286af76b46424
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-04-30 21:05:04 +0100

    fix output queue name

--- src/SFA.DAS.Reservations.Functions.RefreshCourse/Functions/RefreshCourses.cs
+++ src/SFA.DAS.Reservations.Functions.RefreshCourse/Functions/RefreshCourses.cs
@@ -1,13 +1,14 @@
 using System;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using SFA.DAS.Reservations.Infrastructure;
 
 namespace SFA.DAS.Reservations.Functions.RefreshCourse.Functions;
 
 public class RefreshCourses(ILogger<RefreshCourses> logger)
 {
     [Function("RefreshCourses")]
-    [QueueOutput("get-courses")]
+    [QueueOutput(QueueNames.GetCourses)]
     public string Run([TimerTrigger("0 0 0 */1 * *")] TimerInfo myTimer)
     {
         logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
