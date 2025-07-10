03fec7c2a2882adb7b93da40146a0ea2e939508e
Witek <witek.poreda@education.gov.uk>
2025-07-10 15:20:16 +0100

    unit test

---
+++ src/SFA.DAS.Reservations.Data.UnitTests/Repository/AzureSearchReservationIndexRepository/WhenUpdatingAlias.cs
@@ -0,0 +1,29 @@
+﻿using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Reservations.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Reservations.Data.UnitTests.Repository.AzureSearchReservationIndexRepository
+{
+    public class WhenUpdatingAlias
+    {
+        private const string IndexName = "reservations-123";
+
+        [Test, MoqAutoData]
+        public async Task Then_Alias_Is_Created_Or_Updated(
+        [Frozen] Mock<IAzureSearchHelper> azureSearchHelper,
+        Data.Repository.AzureSearchReservationIndexRepository repository)
+        {
+            // Act
+            await repository.UpdateAlias(IndexName);
+
+            // Assert
+            azureSearchHelper.Verify(x => x.UpdateAlias(
+                It.IsAny<string>(),
+                IndexName),
+                Times.Once);
+        }
+    }
+}
