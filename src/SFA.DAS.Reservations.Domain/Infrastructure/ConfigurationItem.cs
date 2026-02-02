ccd0d75223227875e5479904a55779fcab820f35
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2026-02-02 21:33:01 +0000

    update packages

--- src/SFA.DAS.Reservations.Domain/Infrastructure/ConfigurationItem.cs
+++ src/SFA.DAS.Reservations.Domain/Infrastructure/ConfigurationItem.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using Azure;
+using Azure.Data.Tables;
 
-namespace SFA.DAS.Reservations.Domain.Infrastructure
+namespace SFA.DAS.Reservations.Domain.Infrastructure;
+
+public class ConfigurationItem : ITableEntity
 {
-    public class ConfigurationItem : TableEntity
-    {
-        public string Data { get; set; }
-    }
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+    public string Data { get; set; }
 }
--- src/SFA.DAS.Reservations.Infrastructure/AzureServiceBus/AzureQueueService.cs
+++ src/SFA.DAS.Reservations.Infrastructure/AzureServiceBus/AzureQueueService.cs
@@ -1,34 +1,20 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
 using Microsoft.Extensions.Options;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Queue;
 using Newtonsoft.Json;
 using SFA.DAS.Reservations.Domain.Configuration;
 using SFA.DAS.Reservations.Domain.Infrastructure;
 
-namespace SFA.DAS.Reservations.Infrastructure.AzureServiceBus
+namespace SFA.DAS.Reservations.Infrastructure.AzureServiceBus;
+
+public class AzureQueueService(IOptions<ReservationsJobs> options) : IAzureQueueService
 {
-    public class AzureQueueService(IOptions<ReservationsJobs> options) : IAzureQueueService
+    private readonly ReservationsJobs _configuration = options.Value;
+
+    public async Task SendMessage<T>(T message, string queueName) where T : class
     {
-        private readonly ReservationsJobs _configuration = options.Value;
-
-        public async Task SendMessage<T>(T message, string queueName) where T : class
-        {
-            var cloudMessage = new CloudQueueMessage(JsonConvert.SerializeObject(message));
-
-            // Retrieve storage account from connection string.
-            var storageAccount = CloudStorageAccount.Parse(_configuration.AzureWebJobsStorage);
-
-            // Create the queue client.
-            var queueClient = storageAccount.CreateCloudQueueClient();
-
-            // Retrieve a reference to a container.
-            var queue = queueClient.GetQueueReference(queueName);
-
-            // Create the queue if it doesn’t already exist
-            await queue.CreateIfNotExistsAsync();
-
-            await queue.AddMessageAsync(cloudMessage);
-        }
+        var queueClient = new QueueClient(_configuration.AzureWebJobsStorage, queueName);
+        await queueClient.CreateIfNotExistsAsync();
+        await queueClient.SendMessageAsync(JsonConvert.SerializeObject(message));
     }
 }
