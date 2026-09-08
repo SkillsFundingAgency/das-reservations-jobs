ce8854e0ed5f32e2b768390bbf87c10eda913e8f
Paul Graham <paul.graham@coprime.co.uk>
2026-09-08 10:26:07 +0100

    removed IOptions

--- src/SFA.DAS.Reservations.Functions.RefreshCourse/Functions/GetCourseInformation.cs
+++ src/SFA.DAS.Reservations.Functions.RefreshCourse/Functions/GetCourseInformation.cs
@@ -2,14 +2,12 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using SFA.DAS.Reservations.Domain.Configuration;
 using SFA.DAS.Reservations.Domain.RefreshCourse;
 using SFA.DAS.Reservations.Infrastructure;
 
 namespace SFA.DAS.Reservations.Functions.RefreshCourse.Functions;
 
-public class GetCourseInformation(ILogger<GetCourseInformation> logger, IGetCoursesHandler handler, IOptions<ReservationsJobs> options)
+public class GetCourseInformation(ILogger<GetCourseInformation> logger, IGetCoursesHandler handler)
 {
     [Function("GetCourseInformation")]
     [QueueOutput(QueueNames.StoreCourse)]
